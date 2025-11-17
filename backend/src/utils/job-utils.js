046578dc566cea334ea9b11167e079f0c0638bdb
nathsmith-cs <nathanielsmith-cs@gmail.com>
2025-11-16 20:08:44 -0800

    feat - First version of weightJobs method in job-utils

--- backend/src/utils/job-utils.js
+++ backend/src/utils/job-utils.js
@@ -2,26 +2,57 @@
  * @fileoverview Utility functions for parsing job entries and more.
  * @module job-utils
  */
+const { QdrantClient } = require("@qdrant/js-client");
+const { connectWithTunnel } = require("../config/postgres");
 
 /**
- * Turns the JSON of the job entry into a string so the embedding model can understand it better.
- * @param {Object} jobEntry A job entry
- * @returns {string} A string version of jobEntry
- */
-export async function parseJobEntry(jobEntry) {
-    let parsedJobEntry;
-    // TODO: implement parsing logic
-    return parsedJobEntry;
-}
-
-/**
- * @description Takes in jobs and raises each score to the 4th power to decrease the importance of less similar jobs.
- * @param {Array<{id: string|number, score: number}>} topJobs - Array of job objects with id and score properties
- * @returns {Promise<string>} A CSV where each line contains "jobId,weightedScore"
+ *
  */
 
-export async function weightJobs(topJobs) {
-    return topJobs
-        .map((job) => `${job.id},${Math.pow(job.score, 4)}`)
-        .join("\n");
+export async function weightJobs(username, user_id) {
+    //Initialize a client object, establishing connection to Quadrant
+    const client = new QdrantClient({
+        host: "localhost",
+        port: "6333",
+    });
+
+    //Get user vector from quadrant
+    const point = await client.retrieve("users", {
+        ids: [username],
+        with_vectors: true,
+        with_payload: true,
+    });
+
+    //Search job collection using the user vector
+    const weightedJobs = await client.search("jobs", {
+        vector: point[0].vector,
+        limit: 5000,
+        score_threshold: 0,
+    });
+
+    //Insert weightedJobs into Sequel
+    let connection;
+
+    try {
+        connection = await connectWithTunnel();
+        const { pgClient } = connection;
+        console.log();
+
+        for (const job of weightedJobs) {
+            const insertQuery = `INSERT INTO weightedJobs (user_id, vectorname, score, payload)
+                        VALUES ($1,$2,$3,$4)`;
+            const values = [user_id, job.id, job.score, job.payload];
+            await pgClient.query(insertQuery, values);
+            console.log(`job: ${job.id}`);
+        }
+    } catch (err) {
+        console.error("Connection error:", err);
+    }
+
+    if (connection) {
+        console.log();
+        const { cleanup } = connection;
+        cleanup();
+        console.log("Connection closed");
+    }
 }
